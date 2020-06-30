3704e8c5f66c444230d26968a762bb930a7f8ad3
Chris Wood <chris@handsomeweb.com>
2020-06-30 18:21:24 -0500

    Added Required Links in Footer

--- _includes/98-footer.php
+++ _includes/98-footer.php
@@ -10,7 +10,13 @@
                 </div>
                 <div class='col-md-4'>
                     <img class="float-right pb-4" src='_images/uta/name_with_logo.png' src="The University of Texas at Arlington Logo"  />
-                    <p id='footer-links'>Required Link | Required Link | Required Link | Required Link</p>
+                    <p id='footer-links'>
+                        
+                        <a href="https://www.uta.edu/legalaffairs/notices/legal-and-privacy-notice.php">Privacy and Legal Notice</a> |
+                        <a href="https://accessibility.uta.edu/">Accessibility</a> |
+                        <a href="https://www.uta.edu/site-policies">Site Policies</a><br />
+                        <a href="https://www.uta.edu">&copy; <?php echo date("Y"); ?> The University of Texas at Arlington</a>
+                    </p>
                 </div>
             </div>
         </div>
