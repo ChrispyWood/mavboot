27ef0ad0066dc45594c6d814d8fc2877ae124491
Chris Wood <chris@handsomeweb.com>
2020-06-30 20:04:27 -0500

    Footer Social Icons, revision date, and columns

--- _includes/98-footer.php
+++ _includes/98-footer.php
@@ -1,12 +1,17 @@
-<div class='container-fluid' id='footer-area'>
+<div class='container-fluid mb-0 pb-1' id='footer-area'>
     <footer>
         <div class='container'>
             <div class='row'>
                 <div class='col-md-4 pt-2'>
-                    First Column
+                    <h2 class="h4">Sample Lab Page</h2>
+                    <p>615 Nedderman Hall<br />817-272-3769</p>
                 </div>
                 <div class='col-md-4 pt-2'>
-                    Second Column
+                    <span class='social-icons'>
+                        <a href="https://www.facebook.com/mavengineering/"><i class="fab fa-facebook-square"></i></a>
+                        <a href="https://twitter.com/mavengineering"><i class="fab fa-twitter-square"></i></a>
+                        <a href="https://www.instagram.com/mavengineering/"><i class="fab fa-instagram"></i></a>
+                    </span>
                 </div>
                 <div class='col-md-4'>
                     <img class="float-right pb-4" src='_images/uta/name_with_logo.png' src="The University of Texas at Arlington Logo"  />
@@ -15,8 +20,10 @@
                         <a href="https://www.uta.edu/legalaffairs/notices/legal-and-privacy-notice.php">Privacy and Legal Notice</a> |
                         <a href="https://accessibility.uta.edu/">Accessibility</a> |
                         <a href="https://www.uta.edu/site-policies">Site Policies</a><br />
-                        <a href="https://www.uta.edu">&copy; <?php echo date("Y"); ?> The University of Texas at Arlington</a>
+                        <a href="https://www.uta.edu">&copy; <?php echo date("Y"); ?> The University of Texas at Arlington</a><br />
+                        <span class='small'>Updated/reviewed on July 1, 2020</span>
                     </p>
+
                 </div>
             </div>
         </div>
