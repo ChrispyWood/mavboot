6c8e309fdee64fd1a1e097ddebf68b37367fe53d
ChrispyWood <woodc@uta.edu>
2024-06-18 10:33:00 -0500

    Moved ASCII art; Made new HTML for new version

--- _includes/99-bottom.php
+++ _includes/99-bottom.php
@@ -10,5 +10,20 @@ See getbootstrap.com
 <!-- Bootstrap Bundle JS and Mavboot JS -->
 <script src="<?php echo $subpage_depth;?>_js/bootstrap.bundle.min.js"></script>
 <script src="<?php echo $subpage_depth;?>_js/mavboot.js"></script>
+
+<!--
+
+  _    _ _______         ______             _                      _             
+ | |  | |__   __|/\     |  ____|           (_)                    (_)            
+ | |  | |  | |  /  \    | |__   _ __   __ _ _ _ __   ___  ___ _ __ _ _ __   __ _ 
+ | |  | |  | | / /\ \   |  __| | '_ \ / _` | | '_ \ / _ \/ _ \ '__| | '_ \ / _` |
+ | |__| |  | |/ ____ \  | |____| | | | (_| | | | | |  __/  __/ |  | | | | | (_| |
+  \____/   |_/_/    \_\ |______|_| |_|\__, |_|_| |_|\___|\___|_|  |_|_| |_|\__, |
+                                       __/ |                                __/ |
+                                      |___/                                |___/ 
+
+
+-->
+
 </body>
 </html>
